8a2e2ed9af3db1bebf29b117de92b92567020c27
Gabriel007S <gabrielsgb22@gmail.com>
2025-12-16 06:08:18 -0300

    Category

--- src/main/java/com/emperio/course/config/TestConfig.java
+++ src/main/java/com/emperio/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.emperio.course.entities.Category;
 import com.emperio.course.entities.Order;
 import com.emperio.course.entities.User;
 import com.emperio.course.entities.enums.OrderStatus;
+import com.emperio.course.repositories.CategoryRepository;
 import com.emperio.course.repositories.OrderRepository;
 import com.emperio.course.repositories.UserRepository;
 
@@ -23,9 +25,19 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
---
+++ src/main/java/com/emperio/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.emperio.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.emperio.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
