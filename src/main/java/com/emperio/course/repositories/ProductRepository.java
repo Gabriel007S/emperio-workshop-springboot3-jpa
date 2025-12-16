921e721250389ab916ed6661b8e24d1f11ad50e0
Gabriel007S <gabrielsgb22@gmail.com>
2025-12-16 06:34:34 -0300

    Product

---
+++ src/main/java/com/emperio/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.emperio.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.emperio.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
